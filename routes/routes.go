5613d4106fc60d1a148de5038df8698f28fa67ce
Frotten <frottenice@outlook.com>
2025-10-14 21:29:18 +0800

    New Recover

--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,20 @@ package routes
 
 import (
 	"WebApp/logger"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func SetUp() *gin.Engine {
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/Done", func(c *gin.Context) {
+		fmt.Println("Done")
 		c.String(http.StatusOK, "Well done")
 	})
+	zap.L().Info("SetUp Server ...")
 	return r
 }
