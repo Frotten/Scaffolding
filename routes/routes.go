dde2848671a6a1f12782fc3cba0a5a3b1ca6ba31
Frotten <frottenice@outlook.com>
2025-10-14 21:31:59 +0800

    New Recovery

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"WebApp/logger"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,7 +12,6 @@ func SetUp() *gin.Engine {
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 	r.GET("/Done", func(c *gin.Context) {
-		fmt.Println("Done")
 		c.String(http.StatusOK, "Well done")
 	})
 	zap.L().Info("SetUp Server ...")
